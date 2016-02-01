b26f73c0c507e2de62332a0866fd0151a3a7fa7f
Lucas <lucas@agtlucas.com>
2016-02-01 21:34:45 -0200

    Add Trip.swift

---
+++ Drifter/Trip.swift
@@ -0,0 +1,19 @@
+//
+//  Trip.swift
+//  Drifter
+//
+//  Created by Lucas da Silva on 2/1/16.
+//  Copyright © 2016 Vou de ônibus. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Trip: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
