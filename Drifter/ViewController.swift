95ae85d3229686b191a96f8c552b80636ebcb464
Lucas <lucas@agtlucas.com>
2016-02-01 20:30:53 -0200

    Initial Commit

---
+++ Drifter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drifter
+//
+//  Created by Lucas da Silva on 2/1/16.
+//  Copyright © 2016 Vou de ônibus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
