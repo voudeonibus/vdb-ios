6889ac1e496b89cff5a73d0d05e73d90ae741b1b
Lucas <lucas@agtlucas.com>
2016-02-01 21:37:14 -0200

    Update Departure.swift

---
+++ Drifter/Departure.swift
@@ -0,0 +1,16 @@
+//
+//  Departure.swift
+//  Drifter
+//
+//  Created by Lucas da Silva on 2/1/16.
+//  Copyright © 2016 Vou de ônibus. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Departure: Object {
+    
+    var category_day: Int?
+    
+}
