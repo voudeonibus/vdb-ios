3409845f5bd68a6f657b1870a80e8e35b840cb09
Lucas <lucas@agtlucas.com>
2016-02-02 07:22:14 -0200

    Print json file

--- Drifter/ViewController.swift
+++ Drifter/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    let filePath = NSBundle.mainBundle().pathForResource("line", ofType: "json")
+    var readError: NSError?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let data = NSData(contentsOfFile: filePath!) {
+            let json = JSON(data: data, options: NSJSONReadingOptions.AllowFragments, error: nil)
+            print("jsonData:\(json)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
